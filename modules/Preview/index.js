5208a76ed7b10f953e7867955df270f5e5d70f30
Andrew <137772397+Andrew2020jp@users.noreply.github.com>
2026-08-21 21:55:35 +0900

    fix: isolate scrapbook colors from picture book view

--- modules/Preview/index.js
+++ modules/Preview/index.js
@@ -1,4 +1,4 @@
-import { stage, composeMissionPhoto, extractPhotoColor } from "../shared/utils.js?v=suki-no-tane-white-scratchboard-20260821";
+import { stage, composeMissionPhoto, extractPhotoColor } from "../shared/utils.js?v=suki-no-tane-scratchboard-white-20260821";
 import { t } from "../shared/i18n.js";
 
 export const PreviewScreen = {
